ed40f9cf36b576611aabd91a37e8e471175242aa
aluazh55 <marry.teckens@yandex.kz>
2026-03-13 14:36:38 +0500

    Lane Area Detector E2 works and can count cars correctly

---
+++ Script1.py
@@ -0,0 +1,20 @@
+import traci
+
+# Start your simulation
+traci.start(["sumo", "-c", "Test1.sumocfg", "-a", "Test1.add.xml"])
+
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+
+    # 1. Get the current number of vehicles on the South detector
+    count_south = traci.lanearea.getLastStepVehicleNumber("det_south")
+
+    # 2. Get the current number of vehicles on the West detector
+    count_west = traci.lanearea.getLastStepVehicleNumber("det_west")
+
+    # Optional: Get the IDs of the specific vehicles if you need to track them
+    veh_ids = traci.lanearea.getLastStepVehicleIDs("det_south")
+
+    print(f"Step {traci.simulation.getTime()}: South={count_south}, West={count_west}")
+
+traci.close()
